db5b8d42a66a6a477281784e9b1d5c047c9fd486
ctmbl <mabileau.clement@gmail.com>
2023-11-14 23:47:58 +0100

    implement new-blog endpoint to use notify fucntion from webhook.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
 
-from bot import start_bot
+from flask import Flask, request, abort
+
+from webhook import notify
 
 app = Flask("DiscordNotifyApp")
 
 @app.route('/new-blog', methods=["POST"])
 def new_blog():
-    blog = request.get_json()
-    return str(blog)
+    article = request.get_json()
+    keys = ["title", "author", "summary", "url"]
+    for key in keys:
+        if key not in article:
+            app.logger.error("Missing JSON info for new blog post: '%s'", key)
+            abort(400)
 
+    notify(article)
+    app.logger.info("Request correct: notification sent")
+
+    return "Request correct: new article notification sent.\n"
 
 if __name__ == '__main__':
     app.run(host=('0.0.0.0'))
