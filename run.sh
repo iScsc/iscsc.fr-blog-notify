1f7ac2816cc9085caeefc8b47b91b87f411d315a
ctmbl <mabileau.clement@gmail.com>
2023-11-17 15:36:22 +0100

    Add prod mode: ./run.sh prod

--- run.sh
+++ run.sh
@@ -3,10 +3,22 @@
 PATH=$(/usr/bin/getconf PATH || /bin/kill $$)
 NAME="iscscfr-blog-bot"
 
+DETACH=""
+ENTRYPOINT=""
+COMMAND=""
+if [ "$1" = "prod" ]; then
+    DETACH="--detach"
+    ENTRYPOINT="--entrypoint python3"
+    COMMAND="main.py"
+fi
+
 docker container rm ${NAME}
 docker build -t ${NAME} .
 docker run --interactive --tty \
+    $DETACH \
+    $ENTRYPOINT \
     -p 5000:5000 \
     --volume .:/opt/blog_bot \
     --name ${NAME} \
-    ${NAME}:latest
+    ${NAME}:latest \
+    $COMMAND
